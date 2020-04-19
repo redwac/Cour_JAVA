d0ca0d629f8ca4f0f0782ee328b755a308c5cecd
reda <sabbane.reda@gmail.com>
2020-04-19 03:12:43 +0200

    sauv 8

--- Hello_universe/src/helloUniverse.java
+++ Hello_universe/src/helloUniverse.java
@@ -2,6 +2,14 @@ public class helloUniverse {
 
     public static void main(String... args){
         System.out.println("hello universe");
+        int nbrPlanet = 9;
+
+        String c1 = "le nombre de planetes dans le systeme solaire et de : ";
+        System.out.println(c1 + nbrPlanet );
+        nbrPlanet=8;
+        c1="i l ya quelque années le nombre est changer : maintenant il est :"
+        System.out.println(c1 + nbrPlanet );
+
 
     }
 
